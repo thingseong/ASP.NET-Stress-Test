7220cf5b9e47901290f381c55142c1e9a0bb6bd9
thingseong <audtjd1893@gmail.com>
2024-08-30 01:38:30 +0900

    Chore: Gitignore가 없는 관계로 일단 커밋함.

--- TestClient/TestClient/obj/Debug/net8.0/TestClient.AssemblyInfo.cs
+++ TestClient/TestClient/obj/Debug/net8.0/TestClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f632a607f40fe488b5facceb6e8c432cabae203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478e3f2a03b443f96abffc08a0bf213be9343485")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
