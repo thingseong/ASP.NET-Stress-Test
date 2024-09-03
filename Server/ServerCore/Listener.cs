16cf87cce3beb11b50a8d93cb95bea7d01be3755
thingseong <audtjd1893@gmail.com>
2024-09-03 17:32:52 +0900

    Feat : Send 비동기처리

---
+++ Server/ServerCore/Listener.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ServerCore;
+
+public class Listener
+{
+    Socket _listenSocket;
+    private Action<Socket> _onAcceptHandler;
+
+    public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+    {
+        _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        _onAcceptHandler = onAcceptHandler;
+        
+        _listenSocket.Bind(endPoint);
+
+        _listenSocket.Listen(100);
+        
+        SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+        args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+        RegisterAccept(args);
+
+    }
+
+    void RegisterAccept(SocketAsyncEventArgs args)
+    {
+        args.AcceptSocket = null; // args로 Accept를 하기전에 초기화 해줘야함.
+        bool pending = _listenSocket.AcceptAsync(args);
+        if (pending == false)
+            OnAcceptCompleted(null, args);
+        
+    }
+
+    void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+    {
+        if (args.SocketError == SocketError.Success)
+        {
+            // TODO
+            _onAcceptHandler.Invoke(args.AcceptSocket);
+        }
+        else
+            Console.WriteLine("Socket Error: " + args.SocketError);
+        RegisterAccept(args);
+    }
+    
+}
---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;using ServerCore;
+
+Console.WriteLine("Hello, World!");
+
+Listener _listener = new Listener();
+
+static void OnAcceptHandler(Socket clientSocket)
+{
+    try
+    {
+        Session session = new Session();
+        session.Start(clientSocket);
+        
+        byte[] data = Encoding.UTF8.GetBytes("Welcome to the server!");
+        session.Send(data);
+        Thread.Sleep(1000);
+        
+        // session.Disconnect();
+        session.Disconnect();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+    
+    // byte[] bytes = new byte[1024];
+    // int recvBytes = clientSocket.Receive(bytes);
+    // string recvData = Encoding.UTF8.GetString(bytes, 0, recvBytes);
+    //
+    // Console.WriteLine($"[From Client] {recvData}");
+    // byte[] sendBytes = Encoding.UTF8.GetBytes("Welcome to the server!");
+    // clientSocket.Send(sendBytes);
+    //
+    // clientSocket.Shutdown(SocketShutdown.Both);
+    // clientSocket.Close();
+}
+
+try
+{
+    string host = Dns.GetHostName();
+    Console.WriteLine($"Host: {host}");
+    IPHostEntry ipHostEntry = Dns.GetHostEntry(host);
+    IPAddress ipAddress = ipHostEntry.AddressList[0];
+    IPEndPoint endPoint = new IPEndPoint(ipAddress, 7777);
+
+    Console.WriteLine($"IP Address: {ipAddress}");
+    Console.WriteLine($"IP Endpoint: {endPoint}");
+    try
+    {
+        _listener.Init(endPoint, OnAcceptHandler);
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+    while (true)
+    {
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+
+
+
